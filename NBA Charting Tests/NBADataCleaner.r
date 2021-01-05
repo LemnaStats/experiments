e13df286b34117edfc7d3829899a6560c7adfd3b
LemnaStats <61800761+LemnaStats@users.noreply.github.com>
2021-01-04 23:48:34 -0500

    Update NBADataCleaner.r

--- NBA Charting Tests/NBADataCleaner.r
+++ NBA Charting Tests/NBADataCleaner.r
@@ -1,3 +1,4 @@
 CleanStats <- as_tibble(NBAPlayerStats19_20)
 CleanStats <- CleanStats %>% separate(Player, c("Player","PlayerCode"), sep = "\\\\")
-CleanStatsBig5 <- CleanStats %>% filter(nchar(Pos) < 3)
+CleanStatsBig5 <- CleanStats %>% filter(nchar(as.character(Pos)) < 3)
+
