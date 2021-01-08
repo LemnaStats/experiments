e90d256b71be1273a9631b79fc8902f2bfc29a01
LemnaStats <61800761+LemnaStats@users.noreply.github.com>
2021-01-08 16:43:02 -0500

    Update AntimationScratchCode.r

--- AnimatedTest/AntimationScratchCode.r
+++ AnimatedTest/AntimationScratchCode.r
@@ -1,6 +1,5 @@
 ggplot(since3pt, aes(Season, X3PA)) + 
-  geom_smooth() + 
-  geom_point(se=F) +
-  labs(x = '3-Point Shots Attempted', y = 'Season') +
-  transition_time(Season) +
+  geom_line() + 
+  labs(y = "3-Point Shots Attempted", x = "Season") +
+  transition_reveal(along = Season) +
   ease_aes('linear')
